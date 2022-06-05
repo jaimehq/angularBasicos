e4d66fd5d8c1b4db013924eeae5cf94f816e077e
jaimehq <92947496+jaimehq@users.noreply.github.com>
2022-06-05 13:01:13 +0200

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroesModule } from './heroes/heroes.module';
+import { CommonModule } from '@angular/common';
+import { ContadoresModule } from './contadores/contadores.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    CommonModule,
+    ContadoresModule
   ],
   providers: [],
   bootstrap: [AppComponent]
